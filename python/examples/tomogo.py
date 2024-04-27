b0e0f02ba756b1903b3099bc3866d106e88c1e01
Silver Vo <votrinhnhan1403@gmail.com>
2024-04-27 16:53:38 -0500

    update tomogo.py

--- python/examples/tomogo.py
+++ python/examples/tomogo.py
@@ -8,6 +8,13 @@ import time
 import traceback
 import RPi.GPIO as GPIO
 from PIL import Image, ImageDraw, ImageFont
+
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
 from waveshare_epd import epd2in13_V4
 
 def init_gpio():
