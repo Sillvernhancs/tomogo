352cb281dac33959631720d7a38f441e53325696
Silver Vo <votrinhnhan1403@gmail.com>
2024-04-27 18:28:39 -0500

    update files and tomogo.py

--- python/examples/tomogo.py
+++ python/examples/tomogo.py
@@ -9,12 +9,14 @@ import traceback
 import RPi.GPIO as GPIO
 from PIL import Image, ImageDraw, ImageFont
 
+# Define the paths
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-
+monstersdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'monsters')
+# append paths to system path
 if os.path.exists(libdir):
     sys.path.append(libdir)
-
+# import the e ink display 
 from waveshare_epd import epd2in13_V4
 
 def init_gpio():
@@ -28,35 +30,31 @@ def render_text(epd, text, font):
     draw.text((10, 10), text, font=font, fill=0)
     epd.displayPartial(epd.getbuffer(image))
 
+def render_tamagotchi(epd, state):
+    image = Image.open(os.path.join(picdir, f'tamagotchi_{state}.bmp'))  # Load Tamagotchi state image
+    epd.displayPartial(epd.getbuffer(image))
+
 def main():
     logging.basicConfig(level=logging.DEBUG)
-
-    picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-    libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-
-    if os.path.exists(libdir):
-        sys.path.append(libdir)
 
     epd = epd2in13_V4.EPD()
     init_gpio()
 
+    TextFont = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 40)
+
     try:
-        logging.info("epd2in13_V4 Demo")
-        
         epd.init_fast()
         epd.Clear(0xFF)
 
-        font50 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 50)
-
         while True:
-            logging.info("Waiting on Input")
+            # Check button input
             if GPIO.input(5) == GPIO.LOW:
-                logging.info("Button 5 pressed - Rendering YES")
-                render_text(epd, "YES", font50)
+                # render_tamagotchi(epd, "happy")  # Render happy Tamagotchi state
+                render_text(epd,"┗(^o^　)┓三", TextFont)
                 time.sleep(1)
             elif GPIO.input(6) == GPIO.LOW:
-                logging.info("Button 6 pressed - Rendering NO")
-                render_text(epd, "NO", font50)
+                # render_tamagotchi(epd, "sad")    # Render sad Tamagotchi state
+                render_text(epd,"三 ┏ ( ˘ω˘ )┛", TextFont)
                 time.sleep(1)
     except IOError as e:
         logging.info(e)
