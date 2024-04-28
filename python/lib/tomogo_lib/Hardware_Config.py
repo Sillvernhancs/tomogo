ad8efd8b1c295a4bf89168e89007786d5c66ae17
Silver Vo <votrinhnhan1403@gmail.com>
2024-04-28 17:53:19 -0500

    change files dir

--- python/lib/tomogo_lib/Hardware_Config.py
+++
@@ -1,7 +0,0 @@
-#  Tomogo_helper.py
-import RPi.GPIO as GPIO
-
-def init_gpio():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Button on GPIO 5
-    GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Button on GPIO 6
