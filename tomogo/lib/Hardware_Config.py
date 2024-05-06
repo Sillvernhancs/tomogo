db946f2ea5dd7d1060b6840470b3ae2dc6751594
Silver Vo <votrinhnhan1403@gmail.com>
2024-05-05 21:23:36 -0500

    testing buttons

--- tomogo/lib/Hardware_Config.py
+++ tomogo/lib/Hardware_Config.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 
 def init_gpio():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(29, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-    GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-    GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-    GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+    GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+    GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+    GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+    GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
